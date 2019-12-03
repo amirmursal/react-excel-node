f6e07d0ac7ba90c8ccb65b856c88b085db78cae6
Amir Mursal <amir.mursal@communitybrands.com>
2019-12-03 18:26:29 +0530

    added index.js with new app structure

--- server/index.js
+++ server/index.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const path = require("path");
 const cluster = require("cluster");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var xlsx2json = require("xlsx2json");
 const numCPUs = require("os").cpus().length;
 
 const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
+
+var storage = multer.diskStorage({
+  //multers disk storage settings
+  destination: function(req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function(req, file, cb) {
+    var datetimestamp = Date.now();
+    cb(
+      null,
+      file.fieldname +
+        "-" +
+        datetimestamp +
+        "." +
+        file.originalname.split(".")[file.originalname.split(".").length - 1]
+    );
+  }
+});
+
+var upload = multer({
+  //multer settings
+  storage: storage,
+  fileFilter: function(req, file, callback) {
+    //file filter
+    if (
+      ["xls", "xlsx"].indexOf(
+        file.originalname.split(".")[file.originalname.split(".").length - 1]
+      ) === -1
+    ) {
+      return callback(new Error("Wrong extension type"));
+    }
+    callback(null, true);
+  }
+}).single("file");
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -22,9 +59,33 @@ if (!isDev && cluster.isMaster) {
   });
 } else {
   const app = express();
-
+  app.use(bodyParser.json({ limit: "500mb" }));
+  app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
+
+  /** API path that will upload the files */
+  app.post("/upload", function(req, res) {
+    upload(req, res, function(err) {
+      if (err) {
+        res.json({ error_code: 1, err_desc: err });
+        return;
+      }
+      /** Multer gives us file info in req.file object */
+      if (!req.file) {
+        res.json({ error_code: 1, err_desc: "No file passed" });
+        return;
+      }
+      xlsx2json(req.file.path).then(jsonArray => {
+        res.json(jsonArray[0]);
+        //data = jsonArray[0];
+      });
+    });
+  });
+
+  app.get("/getData", function(req, res) {
+    res.json(data);
+  });
 
   // Answer API requests.
   app.get("/api", function(req, res) {
