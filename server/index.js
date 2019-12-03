c5edb7896e715ca7bf969a34ea7ef4cec9ecfb2e
Amir Mursal <amir.mursal@communitybrands.com>
2019-12-03 18:18:54 +0530

    added index.js file for node server

--- server/index.js
+++ server/index.js
@@ -1,74 +1,49 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var multer = require("multer");
-var xlsx2json = require("xlsx2json");
-let data = [];
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
-app.use(bodyParser.json({ limit: "500mb" }));
-app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
 
-var storage = multer.diskStorage({
-  //multers disk storage settings
-  destination: function(req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function(req, file, cb) {
-    var datetimestamp = Date.now();
-    cb(
-      null,
-      file.fieldname +
-        "-" +
-        datetimestamp +
-        "." +
-        file.originalname.split(".")[file.originalname.split(".").length - 1]
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
     );
-  }
-});
-
-var upload = multer({
-  //multer settings
-  storage: storage,
-  fileFilter: function(req, file, callback) {
-    //file filter
-    if (
-      ["xls", "xlsx"].indexOf(
-        file.originalname.split(".")[file.originalname.split(".").length - 1]
-      ) === -1
-    ) {
-      return callback(new Error("Wrong extension type"));
-    }
-    callback(null, true);
-  }
-}).single("file");
-
-/** API path that will upload the files */
-app.post("/upload", function(req, res) {
-  upload(req, res, function(err) {
-    if (err) {
-      res.json({ error_code: 1, err_desc: err });
-      return;
-    }
-    /** Multer gives us file info in req.file object */
-    if (!req.file) {
-      res.json({ error_code: 1, err_desc: "No file passed" });
-      return;
-    }
-    xlsx2json(req.file.path).then(jsonArray => {
-      res.json(jsonArray[0]);
-      //data = jsonArray[0];
-    });
   });
-});
+} else {
+  const app = express();
 
-app.get("/getData", function(req, res) {
-  res.json(data);
-});
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+  // Answer API requests.
+  app.get("/api", function(req, res) {
+    res.set("Content-Type", "application/json");
+    res.send('{"message":"Hello from the custom server!"}');
+  });
 
-app.listen("5000", function() {
-  console.log("running on 5000...");
-});
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function(request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
+  });
+
+  app.listen(PORT, function() {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+}
